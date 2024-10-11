c7e4a5871a473beaddde051e8f2e743103704bd9
cnwank03 <cnwank03@students.cs.ubc.ca>
2024-10-11 14:17:56 -0700

    Implemented the SalesRanking class

--- src/main/model/SalesRanking.java
+++ src/main/model/SalesRanking.java
@@ -8,27 +8,56 @@ public class SalesRanking {
      * EFFECTS: itemList is instantiated as an empty list;
      */
     public SalesRanking(){
-        //stub
+         this.itemList = new ArrayList<>();
     }
     /*
-     * MODIFIES: this
+     * MODIFIES: this, cloth
      * EFFECTS: Adds cloth to the itemList if an instance of an item with them same ID does not already exist in the list;
      *          it modifies the purchaseCount of the existing cloth item
      */
     public void update(Cloth cloth){
-
+        cloth.buy();
+        if (itemList.isEmpty()){
+            this.itemList.add(cloth);
+        }
+        else{
+            int searchId = cloth.getId();
+            boolean canAdd = true;
+            for (Cloth item:this.itemList){
+                if (item.getId() == searchId){
+                    canAdd = false;
+                    break;
+                }
+            }
+            if (canAdd == true){
+                this.itemList.add(cloth);
+            } 
+        }
     }
 
     /*
+     * REQUIRES: itemList.size() >1
      * MODIFIES: this
      * EFFECTS: Sorts itemList from the cloth with the highest purchase count to the lowest
     */
     public void organize(){
-        //Stub
+        int length = this.itemList.size();
+        for (int i =length-1;i>0;i--){
+            int currentCount = itemList.get(i).getPurchaseCount();
+            for (int j=i-1; j>=0;j--){
+                int otherCount = itemList.get(j).getPurchaseCount();
+                if (currentCount > otherCount){
+                    Cloth temp = itemList.get(j);
+                    this.itemList.set(j, itemList.get(i));
+                    this.itemList.set(i,temp);
+                }
+            }
+        }
+        
     }
 
     
     public List<Cloth> getRanking(){
-        return null;
+        return this.itemList;
     }
 }
