d3ed7439b47c69b44ef1aa32e12680b627ce4dbc
cnwank03 <cnwank03@students.cs.ubc.ca>
2024-10-10 21:19:29 -0700

    Added specification and a class level comment for the SalesRanking class

--- src/main/model/SalesRanking.java
+++ src/main/model/SalesRanking.java
@@ -1,13 +1,34 @@
 package model;
 import java.util.List;
+import java.util.ArrayList;
+// Represents the ranking of a list  of items, itemList, based on sales
 public class SalesRanking {
-    private List<Cloth> listItems;
-
+    private List<Cloth> itemList; //List of clothes
+    /*
+     * EFFECTS: itemList is instantiated as an empty list;
+     */
     public SalesRanking(){
+        //stub
+    }
+    /*
+     * MODIFIES: this
+     * EFFECTS: Adds cloth to the itemList if an instance of an item with them same ID does not already exist in the list;
+     *          otherwise, it modifies the purchaseCount of the existing cloth item
+     */
+    public void update(Cloth cloth){
 
     }
 
-    public void update(ClothingInventory stock){
+    /*
+     * MODIFIES: this
+     * EFFECTS: Sorts itemList from the cloth with the highest purchase count to the lowest
+    */
+    public void organize(){
+        //Stub
+    }
 
+    
+    public List<Cloth> getRanking(){
+        return null;
     }
 }
