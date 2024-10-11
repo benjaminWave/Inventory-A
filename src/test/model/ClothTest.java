8cfcc168ab8763e0a9db9ca39c462147dd5c26bd
cnwank03 <cnwank03@students.cs.ubc.ca>
2024-10-10 22:14:16 -0700

    Created tests for the class Cloth

---
+++ src/test/model/ClothTest.java
@@ -0,0 +1,31 @@
+package model;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+public class ClothTest {
+    Cloth testCloth;
+    @BeforeEach
+    public void runBefore() {
+        testCloth = new Cloth("Blue","Shirt",1234);
+    }
+
+    @Test
+    public void testConstructor(){
+        assertEquals("Blue",testCloth.getColor());
+        assertEquals("Shirt",testCloth.getClothType());
+        assertEquals(1234,testCloth.getId());
+        assertEquals(0, testCloth.getPurchaseCount());
+    }
+    @Test
+    public void testBuyOnce(){
+        testCloth.buy();
+        assertEquals(1,testCloth.getPurchaseCount());
+    }
+    public void testBuyMultipleTimes(){
+        testCloth.buy();
+        testCloth.buy();
+        testCloth.buy();
+        assertEquals(3,testCloth.getPurchaseCount());
+    }
+}
+ 
