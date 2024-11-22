0a3d1fb5c0b9281746cf319844541f6696eeb05c
Chidera Benjamin Nwankwo <cnwank03@students.cs.ubc>
2024-11-21 22:05:02 -0800

    added specification for inventoryhandler

--- src/main/ui/InventoryHandler.java
+++ src/main/ui/InventoryHandler.java
@@ -8,24 +8,27 @@ import model.ClothingInventory;
 import persistence.JsonReader;
 import persistence.JsonWriter;
 
+//Represents  a class that handles changes and data retrieval from an instance of a ClothingInventory
 public class InventoryHandler {
     private ClothingInventory inventory;
-    private boolean canRun;
     private JsonWriter jsonWriter;
     private JsonReader jsonReader;
     private static final String JSON_STORE = "./data/inventory.json";
 
+    // EFFECTS: creates a JsonWriter, JsonReader and ClothingInventory
     public InventoryHandler() {
         jsonWriter = new JsonWriter(JSON_STORE);
         jsonReader = new JsonReader(JSON_STORE);
         inventory = new ClothingInventory();
     }
 
+    // EFFECTS: returns viewClothes() string as confirmation of list status
     public String handleListViewing() {
-
         return inventory.viewClothes();
     }
 
+    // MODIFIES: this
+    // EFFECTS: add a new Cloth item to the inventory based on the input
     public void handleAdd(String color, String type, int id) {
         inventory.addCloth(new Cloth(color, type, id));
     }
@@ -34,6 +37,7 @@ public class InventoryHandler {
         return inventory.getInventoryList();
     }
 
+    // EFFECTS: returns displayRanking() string as confirmation of sales status
     public String handleRankViewing() {
         return inventory.displayRanking();
     }
@@ -42,14 +46,23 @@ public class InventoryHandler {
         return inventory.getRanking().getRanking();
     }
 
+    // MODIFIES: this
+    // EFFECTS: removes a cloth item based on id and confirms if this was successful
     public boolean handleRemove(int id) {
         return inventory.removeCloth(id);
     }
 
+    // MODIFIES: this
+    // EFFECTS: buy a cloth item based on id and confirms if this was successful
     public boolean handleBuy(int id) {
         return inventory.buyItem(id);
     }
 
+    /*
+     * MODIFIES: this
+     * EFFECTS: saves the inventory object to file and confirms if this was
+     * successful
+     */
     public boolean handleSave() {
         boolean confirm = false;
         try {
@@ -60,24 +73,29 @@ public class InventoryHandler {
         } catch (IOException e) {
             confirm = false;
         }
-
-
         return confirm;
     }
 
-    public boolean handleLoad(){
+    /*
+     * MODIFIES: this
+     * EFFECTS: replaces the current inventory with one loaded from file and
+     * confirms if this was successful
+     */
+    public boolean handleLoad() {
         boolean confirm = false;
         try {
             inventory = jsonReader.read();
-            confirm = true;  
+            confirm = true;
         } catch (IOException e) {
             confirm = false;
         }
         return confirm;
     }
-    public boolean handleRequest(int id){
+
+    // EFFECTS: requests a cloth item based on id and confirms if this was
+    // successful
+    public boolean handleRequest(int id) {
         return inventory.requestItem(id);
     }
-   
 
 }
