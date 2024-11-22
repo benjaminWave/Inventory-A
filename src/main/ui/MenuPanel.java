c4bd810a3bf2753c25846e726156709ed6ebfedc
Chidera Benjamin Nwankwo <cnwank03@students.cs.ubc>
2024-11-21 22:08:33 -0800

    added specification for MenuPanel

--- src/main/ui/MenuPanel.java
+++ src/main/ui/MenuPanel.java
@@ -4,39 +4,45 @@ import java.awt.*;
 
 import javax.swing.*;
 
+//Represents the main tab of the UI
 public class MenuPanel extends JPanel {
-    private final Color COLOR = Color.white;
+    private final Color mainColor = Color.white;
     JButton viewClothesButton;
     JButton viewSalesButton;
     JButton requestButton;
     ClothGUI parent;
 
+    // EFFECTS: sets this.parent to parent; sets up the MenuPanel's visual
+    // properties and creates a welcome message;
     public MenuPanel(ClothGUI parent) {
         super();
         this.parent = parent;
-        setBackground(COLOR);
+        setBackground(mainColor);
+        JLabel welcomeMessage = new JLabel("Welcome to your inventory. Select one of the following options:");
+        welcomeMessage.setForeground(Color.black);
+        welcomeMessage.setBounds(1, 1, 600, 100);
+        add(welcomeMessage);
         welcome();
         setLayout(null);
     }
 
+    // MODIFIES: this
+    // Sets up the panel; adds a view clothes button, view sales button and request
+    // item button
     private void welcome() {
-
-        JLabel welcomeMessage = new JLabel("Welcome to your inventory. Select one of the following options:");
-        welcomeMessage.setForeground(Color.black);
-        add(welcomeMessage);
         viewClothesButton = new JButton("View the list of clothing available for purchase");
         viewClothesButton.setForeground(Color.white);
         viewClothesButton.setBackground(Color.gray);
         viewClothesButton.addActionListener(parent);
         viewClothesButton.setActionCommand("View List");
-        viewClothesButton.setBounds(1, 1, 600, 100);
+        viewClothesButton.setBounds(1, 103, 600, 100);
         add(viewClothesButton);
         viewSalesButton = new JButton("View the sales ranking of sold clothing");
         viewSalesButton.setForeground(Color.white);
         viewSalesButton.setBackground(Color.gray);
         viewSalesButton.addActionListener(parent);
         viewSalesButton.setActionCommand("View Ranking");
-        viewSalesButton.setBounds(1, 103, 600, 100);
+        viewSalesButton.setBounds(1, 206, 600, 100);
         add(viewSalesButton);
 
         requestButton = new JButton("Request Item");
@@ -44,9 +50,8 @@ public class MenuPanel extends JPanel {
         requestButton.setBackground(Color.gray);
         requestButton.addActionListener(parent);
         requestButton.setActionCommand("Request");
-        requestButton.setBounds(1, 206, 600, 100);
+        requestButton.setBounds(1, 312, 600, 100);
         add(requestButton);
-
 
     }
 
