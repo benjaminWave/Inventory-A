881fc49eeed450f4ee93401fed4aac32fa319467
Chidera Benjamin Nwankwo <cnwank03@students.cs.ubc>
2024-11-21 21:33:42 -0800

    added specification to ClothGUI

--- src/main/ui/ClothGUI.java
+++ src/main/ui/ClothGUI.java
@@ -10,6 +10,7 @@ import model.Cloth;
 import java.awt.*;
 import java.awt.event.*;
 
+//Represents the main UI component 
 public class ClothGUI extends JFrame implements ActionListener, ChangeListener {
 
     private int width = 700;
@@ -26,6 +27,9 @@ public class ClothGUI extends JFrame implements ActionListener, ChangeListener {
     private String mode;
     private RankingContainer rankTab;
 
+    // EFFECTS: invokes the JFrame superclass and sets the size,close operation and
+    // layout to card; creates a JTabbedPane and InventoryHandler; creates the
+    // MenuBar, main Panel and finishes setting up the ClothGUI
     public ClothGUI() {
         super("Clothing Inventory");
         tab = new JTabbedPane();
@@ -33,13 +37,16 @@ public class ClothGUI extends JFrame implements ActionListener, ChangeListener {
         setSize(width, height);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new CardLayout());
-
         createMenuBar();
         createMain();
         finish();
 
     }
 
+    /*
+     * MODIFIES: this
+     * EFFECTS: creates a JMenuBar for the UI with options to load and save
+     */
     private void createMenuBar() {
         this.menuBar = new JMenuBar();
         this.fileMenu = new JMenu("File");
@@ -55,6 +62,10 @@ public class ClothGUI extends JFrame implements ActionListener, ChangeListener {
         setJMenuBar(menuBar);
     }
 
+    /*
+     * MODIFIES: this
+     * EFFECTS: creates and adds the main panel to this
+     */
     private void createMain() {
         menu = new MenuPanel(this);
         tab.addTab("MENU", menu);
@@ -64,12 +75,22 @@ public class ClothGUI extends JFrame implements ActionListener, ChangeListener {
 
     }
 
+    /*
+     * MODIFIES:this
+     * EFFECTS: sets the resizable property to false and makes the GUI visible
+     */
     private void finish() {
 
         setResizable(false);
         setVisible(true);
     }
 
+    /*
+     * REQUIRES: message.length() > 1
+     * MODIFIES: this
+     * EFFECTS: Creates a popup menu that questions the user, yes or no, based on
+     * message
+     */
     private JPopupMenu questionUser(String message) {
         JPopupMenu query = new JPopupMenu();
 
@@ -92,8 +113,9 @@ public class ClothGUI extends JFrame implements ActionListener, ChangeListener {
         return handler;
     }
 
+    // MODIFIES: this
     // EFFECTS: sets the current tab to the List Panel and moves the currentIndex to
-    // 1.
+    // 1
     public void viewItems() {
         listTab = new ListContainer(this);
         tab.addTab("LIST", listTab);
@@ -101,6 +123,11 @@ public class ClothGUI extends JFrame implements ActionListener, ChangeListener {
         listTab.update(handler.handleListViewing(), handler.getClothes());
     }
 
+    /*
+     * MODIFIES: this
+     * EFFECTS: sets the current tab to the Sales Panel and moves the currentIndex
+     * to 1
+     */
     public void viewRanking() {
         rankTab = new RankingContainer(this);
         tab.addTab("SALES", rankTab);
@@ -108,12 +135,19 @@ public class ClothGUI extends JFrame implements ActionListener, ChangeListener {
         rankTab.update(handler.handleRankViewing(), handler.getRanking());
 
     }
-    private void requestItem(){
-requestTab = new RequestContainer(this);
-tab.addTab("REQUEST",requestTab);
-tab.setSelectedIndex(1);
+
+    /*
+     * MODIFIES: this
+     * EFFECTS: sets the current tab to the Request Panel and moves the currentIndex
+     * to 1
+     */
+    private void requestItem() {
+        requestTab = new RequestContainer(this);
+        tab.addTab("REQUEST", requestTab);
+        tab.setSelectedIndex(1);
     }
 
+    // EFFCTS: handles action events
     @Override
     public void actionPerformed(ActionEvent event) {
         String action = event.getActionCommand();
@@ -137,49 +171,57 @@ tab.setSelectedIndex(1);
         } else if (action == "View Ranking") {
 
             viewRanking();
-        }else if (action == "Request"){
+        } else if (action == "Request") {
             requestItem();
         }
     }
 
-   
-
+    // EFFECTS: saves to file and creates a JPopupMenu based on the success of
+    // saving to file
     private void saveFile() {
         JPopupMenu newMenu = new JPopupMenu();
         boolean hasWorked = handler.handleSave();
-        JLabel messageLabel = (hasWorked == true)?new JLabel("Successfully saved to file!"): new JLabel("Could not save to file!");
-        
-       newMenu.add(messageLabel);
-        newMenu.show(this, 50, 50);
-        
-    }
+        JLabel messageLabel = (hasWorked == true) ? new JLabel("Successfully saved to file!")
+                : new JLabel("Could not save to file!");
 
-    private void loadFile(){
-        JPopupMenu newMenu = new JPopupMenu();
-        boolean hasWorked = handler.handleLoad();
-        JLabel messageLabel = (hasWorked == true)?new JLabel("Successfully loaded from file!"): new JLabel("Could not load to file!");
-        
         newMenu.add(messageLabel);
         newMenu.show(this, 50, 50);
-        if (tab.getSelectedIndex()== 1) {
-            if (tab.getSelectedComponent() == listTab){
-               
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: loads from file and creates a JPopupMenu based on the success of
+    // loading file; replaces the currentTab with an updated tab if the current tab
+    // is not the main panel
+    private void loadFile() {
+        JPopupMenu newMenu = new JPopupMenu();
+        boolean hasWorked = handler.handleLoad();
+        JLabel messageLabel = (hasWorked == true) ? new JLabel("Successfully loaded from file!")
+                : new JLabel("Could not load to file!");
+
+        newMenu.add(messageLabel);
+        newMenu.show(this, 50, 50);
+        if (tab.getSelectedIndex() == 1) {
+            if (tab.getSelectedComponent() == listTab) {
+
                 viewItems();
                 tab.remove(1);
             }
-            if (tab.getSelectedComponent() == rankTab){
-               
+            if (tab.getSelectedComponent() == rankTab) {
+
                 viewRanking();
                 tab.remove(1);
             }
-            if (tab.getSelectedComponent() == requestTab){
+            if (tab.getSelectedComponent() == requestTab) {
                 requestItem();
                 tab.remove(1);
             }
         }
-       
+
     }
 
+    // EFFECTS: receives data from the listTab, confirms if the corresponding action
+    // (add, buy and remove) was successful and updates the listTab
     public boolean receiveData(String color, String type, int id, String action) {
         if (action == "added") {
             handler.handleAdd(color, type, id);
@@ -206,6 +248,8 @@ tab.setSelectedIndex(1);
         return false;
     }
 
+    // MODIFIES: this
+    // EFFECTS: If the tab is changed to the main panel, the other tab is removed;
     @Override
     public void stateChanged(ChangeEvent e) {
         if (tab.getSelectedIndex() == 0) {
